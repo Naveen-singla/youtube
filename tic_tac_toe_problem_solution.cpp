94a1c9f23cd0e86fb7103b52252dd3a0323900b5
Naveen-singla <Naveensigla2001@gmail.com>
2021-05-17 11:26:35 +0530

    updated tic toc ans

--- tic_tac_toe_problem_solution.cpp
+++ tic_tac_toe_problem_solution.cpp
@@ -1,59 +1,56 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int query;
-    cin >> query;
-    while (query--)
-    {
-        char board[3][3];
-        int cX = 0, cO = 0, c_ = 0,win_X = 0, win_O = 0;
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                char temp;
-                cin >> board[i][j];
-                temp = board[i][j];
-                if (temp == 'X') cX++;
-                else if (temp == 'O') cO++;
-                else if( temp == '_') c_++;
-            }
-        }
-            if((board[0][0]== 'X') && (board[0][1] == 'X') && (board[0][2] == 'X')) win_X++; 
-            if((board[1][0]== 'X') && (board[1][1] == 'X') && (board[1][2] == 'X')) win_X++; 
-            if((board[2][0]== 'X') && (board[2][1] == 'X') && (board[2][2] == 'X')) win_X++; 
-            if((board[0][0]== 'X') && (board[1][0] == 'X') && (board[2][0] == 'X')) win_X++; 
-            if((board[0][1]== 'X') && (board[1][1] == 'X') && (board[2][1] == 'X')) win_X++; 
-            if((board[0][2]== 'X') && (board[1][2] == 'X') && (board[2][2] == 'X')) win_X++; 
-            if((board[0][0]== 'X') && (board[1][1] == 'X') && (board[2][2] == 'X')) win_X++; 
-            if((board[2][0]== 'X') && (board[1][1] == 'X') && (board[0][2] == 'X')) win_X++; 
+	int query;
+	cin >> query ;
+	while(query--)
+	{
+		char board[3][3];
+		int cx=0,co=0,c_=0,win_x=0,win_o=0;
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				char temp;
+				cin >> board[i][j];
+				temp = board[i][j];
+				if(temp=='X') cx++;
+				else if(temp=='O') co++;
+				else if(temp=='_') c_++;// yes we can write if else statement like this if we have only 1 output line
+			}
+			
+		}
+		
+		if(board[0][0]=='X' && board[0][1]=='X' && board[0][2]=='X') win_x++; //for row 1
+		if(board[1][0]=='X' && board[1][1]=='X' && board[1][2]=='X') win_x++; //for row 2
+		if(board[2][0]=='X' && board[2][1]=='X' && board[2][2]=='X') win_x++; //for row 3
+		if(board[0][0]=='X' && board[1][0]=='X' && board[2][0]=='X') win_x++; //for col 1
+		if(board[0][1]=='X' && board[1][1]=='X' && board[2][1]=='X') win_x++; //for col 2
+		if(board[0][2]=='X' && board[1][2]=='X' && board[2][2]=='X') win_x++; //for col 3
+		if(board[0][0]=='X' && board[1][1]=='X' && board[2][2]=='X') win_x++; // diagonal 1
+		if(board[2][0]=='X' && board[1][1]=='X' && board[0][2]=='X') win_x++; // diagonal 2
 
 
-            if((board[0][0]== 'O') && (board[0][1] == 'O') && (board[0][2] == 'O')) win_O++; 
-            if((board[1][0]== 'O') && (board[1][1] == 'O') && (board[1][2] == 'O')) win_O++; 
-            if((board[2][0]== 'O') && (board[2][1] == 'O') && (board[2][2] == 'O')) win_O++; 
-            if((board[0][0]== 'O') && (board[1][0] == 'O') && (board[2][0] == 'O')) win_O++; 
-            if((board[0][1]== 'O') && (board[1][1] == 'O') && (board[2][1] == 'O')) win_O++; 
-            if((board[0][2]== 'O') && (board[1][2] == 'O') && (board[2][2] == 'O')) win_O++; 
-            if((board[0][0]== 'O') && (board[1][1] == 'O') && (board[2][2] == 'O')) win_O++; 
-            if((board[2][0]== 'O') && (board[1][1] == 'O') && (board[0][2] == 'O')) win_O++; 
-              
-                  
-             if(cO>cX) cout << 3 << "\n";
-            else if(cX-cO>1) cout << 3 << "\n";
-            else if(cX>cO && win_X==1 && win_O==0) cout << 1 << "\n";
-            else if((cX==cO) && win_O==1 && win_X==0) cout << 1 << "\n";
-            else if(c_==0 && (win_O+win_X==0)) cout << 1 << "\n";
-            else if(c_==0 && win_X==2) cout << 1 << "\n";
-            else if (c_>0 && (win_X+win_O==0)) cout << 2 << "\n";
-            else cout << 3 << "\n";
+		if(board[0][0]=='O' && board[0][1]=='O' && board[0][2]=='O') win_o++; //for row 1
+		if(board[1][0]=='O' && board[1][1]=='O' && board[1][2]=='O') win_o++; //for row 2
+		if(board[2][0]=='O' && board[2][1]=='O' && board[2][2]=='O') win_o++; //for row 3
+		if(board[0][0]=='O' && board[1][0]=='O' && board[2][0]=='O') win_o++; //for col 1
+		if(board[0][1]=='O' && board[1][1]=='O' && board[2][1]=='O') win_o++; //for col 2
+		if(board[0][2]=='O' && board[1][2]=='O' && board[2][2]=='O') win_o++; //for col 3
+		if(board[0][0]=='O' && board[1][1]=='O' && board[2][2]=='O') win_o++; // diagonal 1
+		if(board[2][0]=='O' && board[1][1]=='O' && board[0][2]=='O') win_o++; // diagonal 2
 
-             
-        }
-    return 0;
+		if((win_o==1 && win_x==1) || (co>cx) || cx-co>1) cout << "3" <<"\n";
+		else if(win_o==1 && win_x==0 && co==cx) cout << "1" << "\n";
+		else if(win_x==1 && win_o==0 && co<cx) cout << "1" << "\n";
+		else if(win_x==2 && c_==0) cout << "1" << "\n"; // this is the special case so be ware of it
+		else if( (win_x+ win_o==0) && c_==0) cout << "1" <<"\n"; // this is the draw case
+		else if( (win_x + win_o==0) && c_>0 ) cout << "2" << "\n";
+		else cout << "3" << "\n";
+
+		
+	}
 }
